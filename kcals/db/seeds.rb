f6378fc86cc0ac8cf9b60249efad6ba9475e9b58
Kevin Liu <kliu33@buffalo.edu>
2023-02-14 14:18:23 -0500

    testing render

--- kcals/db/seeds.rb
+++ kcals/db/seeds.rb
@@ -8,10 +8,16 @@
 ApplicationRecord.transaction do
     puts "Destroying tables..."
     # Unnecessary if using `rails db:seed:replant`
+    Message.destroy_all
+    Channel.destroy_all
     User.destroy_all
+
     puts "Resetting primary keys..."
     # For easy testing, so that after seeding, the first `User` has `id` of 1
     ApplicationRecord.connection.reset_pk_sequence!("users")
+    ApplicationRecord.connection.reset_pk_sequence!("channels")
+    ApplicationRecord.connection.reset_pk_sequence!("messages")
+
     puts "Creating users..."
     # Create one user with an easy to remember username, email, and password:
     User.create!(
