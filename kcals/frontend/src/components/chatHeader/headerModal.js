667e82b938ec1bed8c94e084b7ae1875759d2415
kliu33 <kliu33@buffalo.edu>
2023-05-03 18:13:34 -0400

    added modal for uploading profile picture

--- kcals/frontend/src/components/chatHeader/headerModal.js
+++ kcals/frontend/src/components/chatHeader/headerModal.js
@@ -3,6 +3,7 @@ import {useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { updateColorMode } from "../../store/session";
+import UploadModal from '../UploadModal/uploadModal';
 
 
 function HeaderModal(props) {
@@ -11,28 +12,8 @@ function HeaderModal(props) {
     const stopProp = (e) => {
       e.stopPropagation()
     }
-    const [photoFile, setPhotoFile] = useState(null)
+    const [hidden, setHidden] = useState(true)
 
-    const handleFile = ({currentTarget}) => {
-        const file = currentTarget.files[0];
-        setPhotoFile(file);
-    }
-
-    const handlePfp = async e =>{
-        e.preventDefault();
-        const formData = new FormData();
-        if (photoFile) {
-            formData.append('user[photo]', photoFile)
-        }
-        const response = await fetch(`/api/users/${sessionUser.id}`, {
-            method: "PATCH",
-            body: formData
-        });
-        if (response.ok) {
-            setPhotoFile(null);
-            dispatch(sessionActions.reload());
-        }
-    }
 
     const handleColor = (e) => {
         dispatch(updateColorMode(sessionUser))
@@ -41,6 +22,9 @@ function HeaderModal(props) {
     const handleSubmit = (e) => {
         return dispatch(sessionActions.logout());
       }
+
+
+    const uploadModal = hidden ? null : <UploadModal setHidden={setHidden}/>
 
     const img = sessionUser.photoUrl ? sessionUser.photoUrl : "https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67"
     return (
@@ -56,12 +40,9 @@ function HeaderModal(props) {
                         <div className="slider round"></div>
                     </label>
                 </div>
-                <div>
-                    <form className="change-pfp" onSubmit={handlePfp}>
-                        <h2>Change Profile Picture</h2>
-                        <input className="file-input" type="file" onChange={handleFile}/>
-                        <button className='update-pfp'> Update Picture</button>
-                    </form>
+                <div className="user-modal-options">
+                    <h2 id='logout' onClick={()=>setHidden(false)}>Upload Profile Photo</h2>
+                    
                     <form onSubmit={handleSubmit}>
                         <div id='logout-div'>
                             <button type='submit' id='logout'>
@@ -71,6 +52,7 @@ function HeaderModal(props) {
                     </form>
                 </div>
             </div>
+            {uploadModal}
         </div>
     );
   }
