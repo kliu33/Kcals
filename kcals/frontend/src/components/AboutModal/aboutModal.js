5dfcfcb10562692705f37bd37b8b4e42af2d96d7
kliu33 <kliu33@buffalo.edu>
2023-05-04 12:54:40 -0400

    added members tab in about modal

--- kcals/frontend/src/components/AboutModal/aboutModal.js
+++ kcals/frontend/src/components/AboutModal/aboutModal.js
@@ -20,6 +20,12 @@ function AboutModal({setAboutHidden, channel}) {
   ];
   const [tab, setTab] = useState('about')
   const users = useSelector(state => state.users)
+  const usersArr = Object.values(users)
+  const usersinroom = usersArr.map((user) => 
+  <div className="user-list-about">
+    <img id='pfp3' src={user?.photoUrl ? user?.photoUrl : 'https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67'} alt=""></img>
+    <span id='pfp5'> {user.firstName} {user.lastName}</span>
+  </div>)
   const sessionUser = useSelector(state => state.session.user);
   const date = new Date(channel.createdAt);
   const creator = Object.values(users).find(user => user.id === channel.authorId)
@@ -54,8 +60,7 @@ function AboutModal({setAboutHidden, channel}) {
         <p>{creator.firstName} {creator.lastName} on {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</p>
     </section>
   </div> 
-  : 
-  <div>Members</div>
+  : usersinroom
 
   return (
     <div id='modal-back' onClick={closeModal}>
