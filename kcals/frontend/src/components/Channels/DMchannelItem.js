9f40b0998de41f1d6436cddb8e9e7c39d729cbdd
kliu33 <kliu33@buffalo.edu>
2023-05-05 10:07:29 -0400

    added pfp to dm list, better styling

--- kcals/frontend/src/components/Channels/DMchannelItem.js
+++ kcals/frontend/src/components/Channels/DMchannelItem.js
@@ -7,14 +7,17 @@ import { useSelector } from 'react-redux';
 
 
 const DMChannelItem = ({dm_channel}) => {
+    const users = useSelector(state => state.users)
     const sessionUser = useSelector(state => state.session.user);
     const recipient = dm_channel.user1.id === sessionUser.id ? dm_channel.user2 : dm_channel.user1
-    
+    const creator = users ? Object.values(users).find(user => user.id === recipient?.id) : null;
+    const img = creator?.photoUrl ? creator?.photoUrl : "https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67"
     return (
         <li>
             <NavLink to={`/dm_channels/${dm_channel.id}`}>
-                <div className="channel-div channel-name">
-                    {recipient.firstName} {recipient.lastName}
+                <div className="channel-div channel-name dm-channel-container">
+                    <img id='pfp9' src={img} alt=""></img>
+                    {recipient.firstName} {recipient.lastName} <span className='you'>{sessionUser.id === recipient.id ? 'you' : ''}</span>
                 </div>
             </NavLink>
         </li>
