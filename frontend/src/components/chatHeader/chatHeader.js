5dadb92e56c38dd35e57401de53c298aee87315b
kliu33 <kliu33@buffalo.edu>
2023-05-09 22:35:20 -0400

    styled search, need to make them redirect now

--- frontend/src/components/chatHeader/chatHeader.js
+++ frontend/src/components/chatHeader/chatHeader.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './chatHeader.css'
 import { useSelector } from 'react-redux';
 import git from '../../imgs/github-logo.png'
+import logo from '../../imgs/logo_copy.png'
 import li from '../../imgs/linkedin.png'
 import al from '../../imgs/angellist.png'
 
@@ -14,9 +15,30 @@ const ChatHeader = (props) => {
     const img = sessionUser.photoUrl ? sessionUser.photoUrl : "https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67"
     const search_channels = channels?.filter(channel => channel.name.toLowerCase().includes(search.toLowerCase()))
     const search_users = users?.filter(user => (user.firstName + user.lastName).toLowerCase().includes(search.toLowerCase()))
-    const channel_list = search_channels.map(channel => <li className='search-item'>{channel.name}</li>)
-    const user_list = search_users.map(user => <li className='search-item'>{user.firstName} {user.lastName}</li>)
-    const search_results = search !== '' ? <ul className='search-results'>{channel_list}{user_list}</ul> : null;
+    let channel_list = search_channels.map(channel => 
+        <li className='search-item'>
+            <span id='hash'>#</span> 
+            {channel.name} 
+            <span id='search-desc'>{channel.description.substring(0,20)}</span>
+        </li>)
+    let user_list = search_users.map(user => 
+        <li className='search-item'>
+            <img id='pfp11' src={user.photoUrl ? user.photoUrl : "https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67"}></img>
+            {user.firstName} {user.lastName} 
+            <span id='active-circle'>🔘</span>
+        </li>)
+    const display = channel_list.length === 0 && user_list.length === 0 ? 
+    <ul className={`search-results ${sessionUser.darkMode ? 'search-results-dark' : ""}`}>
+        <li className='search-item'>
+            <img id='logo' src={logo}></img> 
+            <span>No matching results</span>
+        </li>
+    </ul> 
+    : 
+    <ul className={`search-results ${sessionUser.darkMode ? 'search-results-dark' : ""}`}>
+        {channel_list}{user_list}
+    </ul>
+    const search_results = search !== '' ? display : null;
     return (
         <div className={`header-div ${sessionUser.darkMode ? 'header-dark' : ''}`}>
             <div className="links">
@@ -25,9 +47,10 @@ const ChatHeader = (props) => {
                 <a href="https://wellfound.com/u/kevin-liu-149" target="_blank" rel="noreferrer"><img id="logo" alt="angellist" src={al}/></a>
             </div>
             <div className={`search-div`}>
-                <input placeholder={`Search ${search === '' ? 'server' : search}`} className={`searchBar ${sessionUser.darkMode ? 'search-dark' : ''}`} onChange={(e)=>setSearch(e.target.value)}></input>
+                <input placeholder={`🔍︎ Search ${search === '' ? 'server' : search}`} className={`searchBar ${sessionUser.darkMode ? 'search-dark' : ''}`} value={search} onChange={(e)=>setSearch(e.target.value)}></input>
                 {search_results}
             </div>
+            {search !== '' ? <span className='clear-search' onClick={()=> setSearch('')}>Clear</span> : null}
             <img onClick={props.handleUserModal} id='pfp' alt="pfp" src={img}></img>
         </div>
     )
