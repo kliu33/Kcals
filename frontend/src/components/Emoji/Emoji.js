93a43edba027a4d674a65535ddf9240606386410
kliu33 <kliu33@buffalo.edu>
2023-05-13 09:38:02 -0400

    fixed formatting for emojis

--- frontend/src/components/Emoji/Emoji.js
+++ frontend/src/components/Emoji/Emoji.js
@@ -1,55 +1,85 @@
-import './Emoji.css'
-import { createReaction, deleteReaction } from '../../store/messages';
-import { useSelector } from 'react-redux';
+import "./Emoji.css";
+import { createReaction, deleteReaction } from "../../store/messages";
+import { useSelector } from "react-redux";
 
-const Emoji = ({message}) => {
+const Emoji = ({ message }) => {
+
+  const sessionUser = useSelector((state) => state.session.user);
+  
   const emoji_obj = {
-    'smile': message.reactions.filter(reaction => reaction.emoji === 'smile'),
-    'heart': message.reactions.filter(reaction => reaction.emoji === 'heart'),
-    'thumbs-up': message.reactions.filter(reaction => reaction.emoji === 'thumbs-up'),
-    'thumbs-down': message.reactions.filter(reaction => reaction.emoji === 'thumbs-down'),
-    'laughing': message.reactions.filter(reaction => reaction.emoji === 'laughing'),
-  }
-  const sessionUser = useSelector(state => state.session.user);
+    "smile": message.reactions.filter(
+      (reaction) => reaction.emoji === "smile"
+    ),
+    "heart": message.reactions.filter(
+      (reaction) => reaction.emoji === "heart"
+    ),
+    "thumbs-up": message.reactions.filter(
+      (reaction) => reaction.emoji === "thumbs-up"
+    ),
+    "thumbs-down": message.reactions.filter(
+      (reaction) => reaction.emoji === "thumbs-down"
+    ),
+    "laughing": message.reactions.filter(
+      (reaction) => reaction.emoji === "laughing"
+    ),
+  };
 
   const emoji_dict = (emoji) => {
-    switch(emoji) {
-      case 'smile':
-        return `🙂 ${emoji_obj[emoji].length}`
-      case 'heart':
-        return `💗 ${emoji_obj[emoji].length}`
-      case 'thumbs-up':
-        return `👍 ${emoji_obj[emoji].length}`
-      case 'thumbs-down':
-        return `👎 ${emoji_obj[emoji].length}`
-      case 'laughing':
-        return `😂 ${emoji_obj[emoji].length}`
+    switch (emoji) {
+      case "smile":
+        return `🙂 ${emoji_obj[emoji].length}`;
+      case "heart":
+        return `💗 ${emoji_obj[emoji].length}`;
+      case "thumbs-up":
+        return `👍 ${emoji_obj[emoji].length}`;
+      case "thumbs-down":
+        return `👎 ${emoji_obj[emoji].length}`;
+      case "laughing":
+        return `😂 ${emoji_obj[emoji].length}`;
       default:
         return null;
     }
-  }
+  };
 
   const handleRemoveReact = (k) => {
-    let react = emoji_obj[k].find(react => react.user_id === sessionUser.id)
-    if (react){
-        deleteReaction(react.id);
+    let react = emoji_obj[k].find((react) => react.user_id === sessionUser.id);
+    if (react) {
+      deleteReaction(react.id);
     } else {
-        let new_react = {
-          emoji: k,
-          message_id: message.id,
-          user_id: sessionUser.id
-        };
-        createReaction(new_react)
+      let new_react = {
+        emoji: k,
+        message_id: message.id,
+        user_id: sessionUser.id,
+      };
+      createReaction(new_react);
     }
-  }
+  };
 
-
-return (
-    <div className='emoji-list'>
-        {emoji_obj ? Object.keys(emoji_obj).map(k => emoji_obj[k].length > 0 ? <p key={k} className={`reaction ${sessionUser.darkMode ? 'reaction-dark' : null} ${
-            emoji_obj[k].find(react => react.user_id === sessionUser.id) ? `react-hilight ${sessionUser.darkMode ? 'react-hilight-dark' : ''}` : ''}`} onClick={()=>
-        handleRemoveReact(k)}>{emoji_dict(k)}</p> : null) : null}
+  return (
+    <div className="emoji-list">
+      {emoji_obj
+        ? Object.keys(emoji_obj).map((k) =>
+            emoji_obj[k].length > 0 ? (
+              <p
+                key={k}
+                className={`reaction ${
+                  sessionUser.darkMode ? "reaction-dark" : null
+                } ${
+                  emoji_obj[k].find((react) => react.user_id === sessionUser.id)
+                    ? `react-hilight ${
+                        sessionUser.darkMode ? "react-hilight-dark" : ""
+                      }`
+                    : ""
+                }`}
+                onClick={() => handleRemoveReact(k)}
+              >
+                {emoji_dict(k)}
+              </p>
+            ) : null
+          )
+        : null}
     </div>
-    )};
-  
-  export default Emoji;
+  );
+};
+
+export default Emoji;
--- frontend/src/components/Emoji/EmojiList.js
+++ frontend/src/components/Emoji/EmojiList.js
@@ -1,29 +1,58 @@
-import './Emoji.css'
-import { createReaction} from '../../store/messages';
-import { useSelector } from 'react-redux';
+import "./Emoji.css";
+import { createReaction } from "../../store/messages";
+import { useSelector } from "react-redux";
 
 const EmojiList = (props) => {
-  const sessionUser = useSelector(state => state.session.user);
-
+  const sessionUser = useSelector((state) => state.session.user);
 
   const handleCreateReaction = (emoji) => {
     let new_react = {
       emoji: emoji,
       message_id: props.message.id,
-      user_id: sessionUser.id
+      user_id: sessionUser.id,
     };
-  
-    createReaction(new_react)
+    createReaction(new_react);
   };
 
-return (
-            <ul id='list-of-emojis'>
-                <li className='choose-emoji' key="smile" onClick={()=>handleCreateReaction('smile')}>🙂</li>
-                <li className='choose-emoji' key="heart" onClick={()=>handleCreateReaction('heart')}>💗</li>
-                <li className='choose-emoji' key="thumbs-up" onClick={()=>handleCreateReaction('thumbs-up')}>👍</li>
-                <li className='choose-emoji' key="thumbs-down" onClick={()=>handleCreateReaction('thumbs-down')}>👎</li>
-                <li className='choose-emoji' key="laughing" onClick={()=>handleCreateReaction('laughing')}>😂</li>
-            </ul>
-    )};
-  
-  export default EmojiList;
+  return (
+    <ul id="list-of-emojis">
+      <li
+        className="choose-emoji"
+        key="smile"
+        onClick={() => handleCreateReaction("smile")}
+      >
+        🙂
+      </li>
+      <li
+        className="choose-emoji"
+        key="heart"
+        onClick={() => handleCreateReaction("heart")}
+      >
+        💗
+      </li>
+      <li
+        className="choose-emoji"
+        key="thumbs-up"
+        onClick={() => handleCreateReaction("thumbs-up")}
+      >
+        👍
+      </li>
+      <li
+        className="choose-emoji"
+        key="thumbs-down"
+        onClick={() => handleCreateReaction("thumbs-down")}
+      >
+        👎
+      </li>
+      <li
+        className="choose-emoji"
+        key="laughing"
+        onClick={() => handleCreateReaction("laughing")}
+      >
+        😂
+      </li>
+    </ul>
+  );
+};
+
+export default EmojiList;
