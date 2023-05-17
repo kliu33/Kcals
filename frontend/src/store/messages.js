9c62df2830e586984f23f0ad3a7a3ac0b69ef134
kliu33 <kliu33@buffalo.edu>
2023-05-17 11:20:27 -0400

    testing emoji fix on heroku

--- frontend/src/store/messages.js
+++ frontend/src/store/messages.js
@@ -134,7 +134,9 @@ export const messagesReducer = (state = {}, action) => {
       const updatedMessage = action.message;
       return { ...state, [updatedMessage.id]: updatedMessage };
     case RECEIVE_REACTION:
-      newState[action.reaction.message_id].reactions.push(action.reaction)
+      if (!newState[action.reaction.message_id].reactions.includes(action.reaction)){
+        newState[action.reaction.message_id].reactions.push(action.reaction)
+      }
       return newState
     case REMOVE_REACTION:
       const messageId = action.reaction.message_id;
